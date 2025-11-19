e8ee32755a0dd2f070c9727ff95c22a5dbb6faf5
Joantha21 <thanhkiethuynh21022005@gmail.com>
2025-11-19 12:36:45 -0700

    normalize layer

--- LLM.py
+++ LLM.py
@@ -1,5 +1,19 @@
+import math
+import inspect
+from dataclasses import dataclass
 import torch as t
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import torch.nn as nn
+import torch.nn.functional as F
+
+class LayerNorm(nn.Module): #normalize layer with bias = False
+    def __init__(self, ndim, bias):
+        super().__init__()
+        self.weight = nn.Parameter(t.ones(ndim))
+        self.bias = nn.Parameter(t.zeros(ndim)) if bias else None
+
+    def forward(self, input):
+        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+    
+
